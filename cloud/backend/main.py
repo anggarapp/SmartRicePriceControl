45c82d720df1d4f7f54f059bf5f6ae1f743fec97
anggarapp <anggaraperpe@gmail.com>
2021-06-01 16:15:18 +0700

    update parameters

--- cloud/backend/main.py
+++ cloud/backend/main.py
@@ -1,13 +1,29 @@
+# input_suhu_ratarata bulan 1
+# input_suhu_ratarata bulan 2
+# input_suhu_ratarata bulan 3
+# input_suhu_ratarata bulan 4
+# input_kelembaban_ratarata bulan 1
+# input_kelembaban_ratarata bulan 2
+# input_kelembaban_ratarata bulan 3
+# input_kelembaban_ratarata bulan 4
+# input_curah_hujan bulan 1
+# input_curah_hujan bulan 2
+# input_curah_hujan bulan 3
+# input_curah_hujan bulan 4
+# input_lama_penyinaran
+# input_luas_lahan
+
 import h5py
 import gcsfs
-import numpy
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow import keras
 from flask import Flask, request, jsonify
 
+
 model_location = 'gs://rice_price_dev/ml_models/test_model_r5.h5'
-input_fit_data = [[0,0,0,0],[9,9,9,9]] ## need to change this
-output_fit_data = [[1637.76],[115862.16]]
+input_fit_data = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                  [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9]]  # need to change this
+output_fit_data = [[1637.76], [115862.16]]
 input_scaler = MinMaxScaler()
 output_scaler = MinMaxScaler()
 input_scaler.fit(input_fit_data)
@@ -15,30 +31,32 @@ output_scaler.fit(output_fit_data)
 
 app = Flask(__name__)
 
-@app.route('/api/price_predict', methods =['GET'] )
+
+@app.route('/api/price_predict', methods=['GET'])
 def predict():
     try:
         data1 = float(request.args.get('param1'))
         data2 = float(request.args.get('param2'))
         data3 = float(request.args.get('param3'))
-        data4 = float(request.args.get('param4'))  
-        data = [[data1,data2,data3,data4]]
+        data4 = float(request.args.get('param4'))
+        data = [[data1, data2, data3, data4]]
     except:
-        data =[[]]
+        data = [[]]
     results = {
-        "Produksi":0.0,
+        "Produksi": 0.0,
     }
     print(data)
 
-    ##checking input
+    # checking input
     if len(data[0]) != 4:
         return jsonify(results)
     for nilai in data[0]:
         if str(type(nilai)) != "<class 'float'>":
             return jsonify(results)
 
-    FS = gcsfs.GCSFileSystem(project='Smart Food Prices Control', token='test_assets/cred.json')
-    
+    FS = gcsfs.GCSFileSystem(
+        project='Smart Food Prices Control', token='test_assets/cred.json')
+
     with FS.open(model_location, 'rb') as model_file:
         model_gcs = h5py.File(model_file, 'r')
         test_Model = keras.models.load_model(model_gcs)
@@ -48,9 +66,10 @@ def predict():
     prediction = output_scaler.inverse_transform(raw_prediction)
 
     results["Produksi"] = float(prediction[0][0])
-    
+
     json_results = jsonify(results)
     return json_results
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port='8080',debug=True)
+    app.run(host='0.0.0.0', port='8080', debug=True)
